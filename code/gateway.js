3ee5ac6e7787cc0af926eca195196f24611047f7
潘钰文 <panyw@edhic.com>
2026-05-19 11:27:28 +0800

    testhook

--- code/gateway.js
+++ code/gateway.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { PORT } = require('./lib/config.js');
 const { setupRoutes } = require('./lib/routes.js');
 
-
 const app = express();
 app.use(express.json());
 
